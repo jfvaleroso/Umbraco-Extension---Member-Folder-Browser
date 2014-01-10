e22d85411d087e0a4cb54fa336bdab989d58cc33
Jeffrey Valeroso <jeffrey.valeroso@isobar.com>
2014-01-10 18:10:19 +0800

    Initial upload

---
+++ Application.DataType/Application.DataType/memberFolderBrowser/DataTypeMemberFolderBrowser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.cms.businesslogic.datatype;
+using umbraco.interfaces;
+
+namespace Application.DataType.memberFolderBrowser
+{
+    public class DataTypeMemberFolderBrowser : BaseDataType, IDataType
+    {
+        private IDataEditor _editor;
+        private DefaultData _baseData;
+        private IDataPrevalue _prevalueeditor;
+
+        public override IDataEditor DataEditor
+        {
+            get { return _editor ?? (_editor = new MemberFolderBrowser()); }
+        }
+
+        public override IData Data
+        {
+            get { return _baseData ?? (_baseData = new DefaultData(this)); }
+        }
+
+        public override Guid Id { get { return new Guid("CCCD4AE9-F399-4ED2-8038-2E88D19E810D"); } }
+
+        public override string DataTypeName { get { return "Member Folder browser"; } }
+
+        public override IDataPrevalue PrevalueEditor
+        {
+            get { return _prevalueeditor ?? (_prevalueeditor = new umbraco.cms.businesslogic.datatype.DefaultPreValueEditor(this, false)); }
+        }
+    }
+}
---
+++ Application.DataType/Application.DataType/memberFolderBrowser/memberFolderBrowser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using umbraco.interfaces;
+using umbraco.IO;
+
+namespace Application.DataType.memberFolderBrowser
+{
+    public class MemberFolderBrowser : WebControl, IDataEditor
+    {
+
+        public Control Editor { get { return this; } }
+
+        public virtual bool TreatAsRichTextEditor { get { return false; } }
+        public bool ShowLabel { get { return false; } }
+
+        public void Save()
+        {
+        }
+
+        private readonly string _usercontrolPath = Umbraco.Core.IO.IOHelper.ResolveUrl(Umbraco.Core.IO.SystemDirectories.Umbraco) + "/dashboard/MemberDashboardFolderBrowser.ascx";
+        
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            base.Controls.Add(new UserControl().LoadControl(_usercontrolPath));
+        }
+        protected override void Render(HtmlTextWriter writer)
+        {
+            const string styles = "<style>.umbFolderBrowser { position: relative; min-height: 440px; } .upload-panel { top: 80px; margin: 0 0 0 -200px; } .umbFolderBrowser .filter, .umbFolderBrowser .thumb-sizer { top: 0; }</style>";
+            writer.WriteLine(styles);
+            base.Render(writer);
+        }
+    }
+}
